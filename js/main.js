cdda63d62e65219252af913c049c38268b10d411
LonesG <leona.griff@gmail.com>
2021-10-06 19:41:13 +0000

    Add lizard and spock game options

--- js/main.js
+++ js/main.js
@@ -1,18 +1,150 @@
-let choices = document.querySelectorAll('.choice');
-let score = document.getElementById('score');
-let result = document.getElementById('result');
-let restart = document.getElementById('restart');
-let modal = document.querySelectorAll('.modal');
-let scoreboard = { 
-    player: 0,
-    computer: 0
-}
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
+const scoreboard = {
+  player: 0,
+  computer: 0
+};
 
-//Play Game
+// Play game
 function play(e) {
-    restart.style.display = 'inline-block';
-    let playerChoice = e.target.id;
+  restart.style.display = 'inline-block';
+  const playerChoice = e.target.id;
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+  showWinner(winner, computerChoice);
 }
 
-//Event Listeners
+// Get computers choice
+function getComputerChoice() {
+  const rand = Math.random();
+  if (rand < 0.2) {
+    return 'rock';
+  } else if (rand <= 0.4) {
+    return 'paper';
+  } else if (rand <= 0.6) {
+    return 'scissors';
+  } else if (rand <= 0.8) {
+      return 'lizard';
+  } else return 'spock'
+
+}
+
+// Get game winner
+function getWinner(p, c) {
+	if (p === c) {
+    return "draw";
+	} else if (p === "rock") {
+		if (c === "paper") {
+      return "computer";
+		} else if (c === "scissors") {
+      return "player";
+		} else if (c === "lizard") {
+			return "player";
+		} else {
+			return "computer";
+		}
+	} else if (p === "paper") {
+		if (c === "rock") {
+			return "player";
+		} else if (c === "scissors") {
+			return "computer";
+		} else if (c === "lizard") {
+			return "computer";
+		} else {
+			return "player";
+		}
+	}else if (p === "scissors") {
+		if (c === "rock") {
+			return "computer";
+		} else if (c === "paper") {
+			return "player";
+		} else if (c === "lizard") {
+			return "player";
+		} else {
+			return "computer";
+		}
+	} else if (p === "lizard") {
+		if (c === "rock") {
+			return "computer";
+		} else if (c === "paper") {
+			return "player";
+		} else if (c === "scissors") {
+			return "computer";
+		} else {
+			return "player";
+		}
+	} else if (p === "spock") {
+		if (c === "rock") {
+			return "player";
+		} else if (c === "paper") {
+			return "computer";
+		} else if (c === "scissors") {
+			return "player";
+		} else {
+			return "computer";
+		}
+	}
+};
+
+function showWinner(winner, computerChoice) {
+  if (winner === 'player') {
+    // Inc player score
+    scoreboard.player++;
+    // Show modal result
+    result.innerHTML = `
+      <h1 class="text-win">You Win</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+        computerChoice.slice(1)}</strong></p>
+    `;
+  } else if (winner === 'computer') {
+    // Inc computer score
+    scoreboard.computer++;
+    // Show modal result
+    result.innerHTML = `
+      <h1 class="text-lose">You Lose</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+        computerChoice.slice(1)}</strong></p>
+    `;
+  } else {
+    result.innerHTML = `
+      <h1>It's A Draw</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+        computerChoice.slice(1)}</strong></p>
+    `;
+  }
+  // Show score
+  score.innerHTML = `
+    <p>Player: ${scoreboard.player}</p>
+    <p>Computer: ${scoreboard.computer}</p>
+    `;
+
+  modal.style.display = 'block';
+}
+
+// Restart game
+function restartGame() {
+  scoreboard.player = 0;
+  scoreboard.computer = 0;
+  score.innerHTML = `
+    <p>Player: 0</p>
+    <p>Computer: 0</p>
+  `;
+}
+
+// Clear modal
+function clearModal(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
+// Event listeners
 choices.forEach(choice => choice.addEventListener('click', play));
+window.addEventListener('click', clearModal);
+restart.addEventListener('click', restartGame);
